d21c4aff6c812a30ccff62cb15025d5021806de9
Emanuel Calderoni <emanuelcalderoni@mac-mini-de-emanuel.local>
2017-09-25 20:14:14 -0300

    agrega comentario

--- GithubProject6/ViewController.swift
+++ GithubProject6/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        //agregar unos comentarios: que lindo dia tuvimos !
     }
 
 
